e0f8b86d62e328e71d2ab066502bb4424c37955c
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-04-06 21:15:16 +0500

    add: layout pug

--- Todo-app/app.js
+++ Todo-app/app.js
@@ -1,23 +1,16 @@
-const path = require("path");
-const fs = require('fs');
+const express = require('express')
+const app = express()
 
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const PORT = 8000
 
-// routes
-const notes = require("./routes/notes");
-const comments = require("./routes/comments");
-const getCollection = require("./utils").getCollection;
+app.set('view engine', 'pug')
 
-// serving static files
-app.use(express.static('public'));
+// http://localhost:8000
+app.get('/', (req, res) => {
+    res.render('layout')
+})
+app.listen(PORT, (err) => {
+    if (err) throw err
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
-
-// setting template engine
-app.set("view engine", "pug");
+    console.log(`This app is running on port ${PORT}`)
+})
